1272f6852e902aa58ff5846d9cafec6666edeae3
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-01-28 16:03:18 +0100

    Eternal shuffle queue

--- neuralnets/data/samplers.py
+++ neuralnets/data/samplers.py
@@ -1,10 +1,26 @@
 import torch
 import numpy
+import queue
+
+
+class _EternalShuffleQueue(queue.Queue):
+    def __init__(self, items):
+        super(_EternalShuffleQueue, self).__init__()
+        self.items = items
+    
+    def pop(self):
+        if super().empty():
+            numpy.random.shuffle(self.items)
+            for item in self.items:
+                super().put(item)
+
+        return super().get()
 
 class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
     """
     This sampler provides mini-batches with a balanced amount of instances per class.
     """
+
 
     def __init__(self, targets, batch_size):
         """Initialize balanced batch sampler.
@@ -21,7 +37,7 @@ class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
         targets = numpy.array(targets)
         for i in self.classes:
             # get all indices for class i
-            self.idx_per_class[i] = numpy.nonzero(targets == i)[0]
+            self.idx_per_class[i] = _EternalShuffleQueue(numpy.nonzero(targets == i)[0])
 
     def __iter__(self):
         for _ in range(self.length):
@@ -29,7 +45,7 @@ class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
                 list(
                     map(
                         # map class index to random dataset index of that class
-                        lambda x: numpy.random.choice(self.idx_per_class[x]), 
+                        lambda x: self.idx_per_class[x].pop(), 
                         # randomly sample class indices
                         numpy.random.choice(self.classes, self.batch_size)
                     )
