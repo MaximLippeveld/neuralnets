b8644fb6112d15ef3f29a2b7115efefbae1b4997
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-03-19 11:57:07 +0100

    Switch to selu

--- neuralnets/networks/convolutional/convnet.py
+++ neuralnets/networks/convolutional/convnet.py
@@ -7,12 +7,10 @@ class ConvNet(nn.Module):
 
         self.extractor = nn.Sequential(
             nn.Conv2d(image_shape[0], 8, 3, 1, 1),
-            nn.BatchNorm2d(8),
-            nn.ReLU(),
+            nn.SELU(),
             nn.MaxPool2d(2),
             nn.Conv2d(8, 16, 3, 1, 1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
+            nn.SELU(),
             nn.MaxPool2d(2),
             nn.Flatten()
         )
@@ -23,8 +21,7 @@ class ConvNet(nn.Module):
         self.classifier = nn.Sequential(
             nn.Dropout(dropout_rate),
             nn.Linear(flat.shape[1], 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
+            nn.SELU(),
             nn.Dropout(dropout_rate),
             nn.Linear(128, num_classes)
         )
@@ -32,5 +29,5 @@ class ConvNet(nn.Module):
     def forward(self, x):
         x = self.extractor(x)
         x = self.classifier(x)
-
+        
         return x
